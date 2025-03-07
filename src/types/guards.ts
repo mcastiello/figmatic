77e36d8f09363b94d0f93bba116cb6088510179f
Marco Castiello <marco.castiello@gmail.com>
2025-03-07 13:44:50 +0000

    Add method to resolve tokens values

--- src/types/guards.ts
+++ src/types/guards.ts
@@ -1,9 +1,11 @@
-import { NodeType, PropertyTypes } from "./enumerators";
+import { NodeType } from "./enumerators";
 import { GenericNode, GenericNodeData, NodeDefinitionData, StyledNodeData } from "./nodes";
 import { NodesDataMap, NodesMap } from "./map";
+import { VariableAlias, VariableValue } from "./properties";
 
 export const isNodeType = (type: string): type is NodeType => Object.values<string>(NodeType).includes(type);
-export const isPropertyType = (type: string): type is NodeType => Object.values<string>(PropertyTypes).includes(type);
+export const isVariableAlias = (value: VariableValue | VariableAlias): value is VariableAlias =>
+  typeof value === "object" && Object.hasOwn(value, "type");
 export const isNodeData = <Data extends GenericNode>(data?: Record<string, unknown>): data is Data =>
   typeof data?.type === "string" && isNodeType(data.type);
 export const isTypedNodeData = <Type extends keyof NodesDataMap>(
