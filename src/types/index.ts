2c94af4f9a53d8ee9a684060a861a5c4954b708f
Marco Castiello <marco.castiello@gmail.com>
2025-03-07 14:07:54 +0000

    Expose type guards

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,4 +2,5 @@ export * from "./enumerators";
 export * from "./map";
 export * from "./nodes";
 export * from "./properties";
+export * from "./guards";
 export { FigmaticEvents, FigmaticSeverity } from "./events";
